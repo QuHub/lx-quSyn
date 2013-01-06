47571d6c8a9e74ac12b9c6ad6a94787c10650977
Maher Hawash <gmhawash@gmail.com>
2013-01-06 07:42:24 -0800

    test incomplete function copy

--- tests/CoveredSetPatitionTest.cpp
+++ tests/CoveredSetPatitionTest.cpp
@@ -45,7 +45,6 @@ void CoveredSetPatitionTest::test_copy_terms_in_function() {
 
   CoveredSetPartition::initialize(&function, 2);
   CoveredSetPartition csp;
-  function.dump();
 
   unsigned long p[] = {0, 2, 1, 4, 5, 3, 7, 6, 8, 9, 10, 12, 11, 14, 15, 13};
   vector<unsigned long> expected(p, p+16);
@@ -64,23 +63,23 @@ void CoveredSetPatitionTest::test_copy_terms_in_function() {
 
 void CoveredSetPatitionTest::test_copy_terms_in_incomplete_function() {
   Function function;
-  function.load_file("./tests/fixtures/function-4.xqs");
+  function.load_file("./tests/fixtures/function-4-incomplete.xqs");
 
   CoveredSetPartition::initialize(&function, 2);
   CoveredSetPartition csp;
-  function.dump();
 
   unsigned long p[] = {0, 2, 1, 4, 5, 3, 7, 6, 8, 9, 10, 12, 11, 14, 15, 13};
-  vector<unsigned long> expected(p, p+16);
+  unsigned long pin[] = {0, 2, 4, 5, 3, 7, 8, 10, 12, 15};
+  vector<unsigned long> expected(pin, pin+10);
   csp.copy_terms_in_function(p);
 
   unsigned long *pInput = csp.inputs();
-  vector<unsigned long> actual(pInput, pInput + 16);
+  vector<unsigned long> actual(pInput, pInput + 10);
   assert_equal_vectors(expected, actual);
 
-  unsigned long pout[] = {7, 6, 15, 0, 13, 14, 10, 5, 9, 12, 8, 4, 2, 3, 1, 11};
-  expected.assign(pout, pout+16);
+  unsigned long pout[] = {7, 6, 0, 13, 14, 10, 9, 8, 4, 1};
+  expected.assign(pout, pout+10);
   unsigned long *pOutput = csp.outputs();
-  actual.assign(pOutput, pOutput + 16);
+  actual.assign(pOutput, pOutput + 10);
   assert_equal_vectors(expected, actual);
 }
