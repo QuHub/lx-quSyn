445482f3ee87454e41f5ecfa1adf35b7167a163f
Maher Hawash <gmhawash@gmail.com>
2013-01-06 21:42:51 -0800

    test Synthesizer#cost

--- tests/SynthesizerTest.cpp
+++ tests/SynthesizerTest.cpp
@@ -12,13 +12,14 @@
 
 class SynthesizerMock : public Synthesizer {
   public:
-    void stub(unsigned long *pcontrol, unsigned long *ptarget, int num_gates) {
+    void stub(unsigned long *pcontrol, unsigned long *ptarget, int num_gates, int num_bits) {
     m_pcontrol = pcontrol;
     m_ptarget = ptarget;
     m_num_gates = num_gates;
-    m_num_bits = 2;
+    m_num_bits = num_bits;
   }
   unsigned long propogate(unsigned long term) {return Synthesizer::propogate(term);}
+  unsigned long cost() {return Synthesizer::cost();}
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SynthesizerTest);
@@ -39,17 +40,26 @@ void SynthesizerTest::test_propogate() {
   SynthesizerMock syn;
   unsigned long control[] = {0b1100, 0b1001, 0b0101};
   unsigned long target[] =  {0b0001, 0b0100, 0b1000};
-  syn.stub(control, target, 3);
+  syn.stub(control, target, 3, 4);
   assert_equal(0b1011UL, syn.propogate(0b1110));
 
   unsigned long control2[] = {0b1100101, 0b1001001, 0b1110101};
   unsigned long target2[] =  {0b0001000, 0b0000100, 0b0001000};
-  syn.stub(control2, target2, 3);
+  syn.stub(control2, target2, 3, 7);
 
   assert_equal(0b1110111UL, syn.propogate(0b1111011));
 }
 
 void SynthesizerTest::test_cost() {
+  SynthesizerMock syn;
+  unsigned long control[] = {0b1100, 0b1001, 0b0101};
+  syn.stub(control, NULL, 3, 4);
+  assert_equal(15UL, syn.cost());
+
+  unsigned long control2[] = {0b11100, 0b01001, 0b10101};
+  syn.stub(control2, NULL, 3, 5);
+  assert_equal(31UL, syn.cost());
+
 }
 
 void SynthesizerTest::test_control_lines() {
